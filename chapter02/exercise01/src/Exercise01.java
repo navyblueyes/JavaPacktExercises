3395548ee3ba71a53c62f3325171663e57965359
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2022-04-05 13:00:55 -0500

    first commit

---
+++ chapter02/exercise01/src/Exercise01.java
@@ -0,0 +1,9 @@
+public class Exercise01 {
+    public static void main(String[] args) {
+        boolean happy = true;
+        boolean sad = false;
+        if (happy)
+            System.out.println("I am happy");
+        if (sad) System.out.println("The variable sad is true");
+    }
+}
